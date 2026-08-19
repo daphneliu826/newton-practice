46664e559da1df2a5e4a43026be76529de66ae9f
Sixian Liu <sixianliu@berkeley.edu>
2026-08-19 14:16:17 -0700

    Add documentation and format Newton module.

--- newton.py
+++ newton.py
@@ -1,20 +1,44 @@
 import numpy as np
 
+
+# Estimate the first and second derivatives.
 def derivative(f, x, eps=1e-5):
+    """Estimate the first derivative of a function using finite differences."""
     return (f(x + eps) - f(x)) / eps
 
+
 def second_derivative(f, x, eps=1e-5):
+    """Estimate the second derivative of a function using finite differences."""
     return derivative(lambda y: derivative(f, y, eps), x, eps)
-    
+
+
 def optimize(x0, f, tol=1e-6, max_iter=100):
+    """Minimize a univariate function using Newton's method.
+
+    Parameters
+    ----------
+    x0 : float
+        Starting value for the optimization.
+    f : function
+        Function to minimize.
+    tol : float
+        Tolerance for the stopping criterion.
+    max_iter : int
+        Maximum number of iterations.
+
+    Returns
+    -------
+    x0 : float
+        Estimated location of the minimum.
+    iteration : int
+        Number of iterations performed.
+    """
     for iteration in range(max_iter):
-        
         fp = derivative(f, x0)
         fpp = second_derivative(f, x0)
 
-        if np.abs(fp)< tol:
+        if np.abs(fp) < tol:
             break
-            
+
         x0 = x0 - fp / fpp
     return x0, iteration
-
